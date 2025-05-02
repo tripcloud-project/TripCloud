e6b0888f32736c69e34125a8c8b2afa72e42275f
김성진 <kimsungjin927@gmail.com>
2025-05-02 15:44:19 +0900

    feat: Activity Entity 작성

--- backend/src/main/java/com/ssafy/project/domain/activity/entity/Activity.java
+++ backend/src/main/java/com/ssafy/project/domain/activity/entity/Activity.java
@@ -1,5 +1,23 @@
 package com.ssafy.project.domain.activity.entity;
 
-public class Activity {
+import com.ssafy.project.domain.common.entity.BaseEntity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper=false)
+
+public class Activity extends BaseEntity{
+	private @NonNull Long activityId;
+	private @NonNull Long memberId;
+	private @NonNull String activity;
+	private @NonNull Integer quantity;
 }
