7d331ae0ec169f406b61cf8a074d271326d742a3
김성진 <kimsungjin927@gmail.com>
2025-05-02 14:21:05 +0900

    feat: 게시글 엔티티 작성

--- backend/src/main/java/com/ssafy/project/domain/board/entity/Post.java
+++ backend/src/main/java/com/ssafy/project/domain/board/entity/Post.java
@@ -1,5 +1,25 @@
 package com.ssafy.project.domain.board.entity;
 
-public class Post {
+import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Post {
+	private @NonNull Long postId;
+	private @NonNull Long memberId;
+	private @NonNull String title;
+	private @NonNull String content;
+	private String filePath;
+	private @NonNull LocalDateTime createdAt;
+	private @NonNull LocalDateTime modifiedAt;
+	private @NonNull Boolean isDeleted;
 }
