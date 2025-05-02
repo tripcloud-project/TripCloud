4d481055b1bc024d9be5f4c0ac5dc6db3d5ed4d9
GK <weeeeek2@naver.com>
2025-05-02 16:05:05 +0900

    feat: Add Attraction Entity

--- backend/src/main/java/com/ssafy/project/domain/attraction/entity/Attraction.java
+++ backend/src/main/java/com/ssafy/project/domain/attraction/entity/Attraction.java
@@ -1,5 +1,23 @@
 package com.ssafy.project.domain.attraction.entity;
 
-public class Attraction {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Attraction {
+	private @NonNull Long attractionId;
+	private @NonNull String attractionCategoryName;
+	private @NonNull String sido;
+	private @NonNull String sigungu;
+	private @NonNull String eupmyeondong;
+	private @NonNull String name;
+	private @NonNull String description;
+	private @NonNull String latitude;
+	private @NonNull String longitude;
 }
