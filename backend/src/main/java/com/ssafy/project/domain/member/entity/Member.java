4d156be85ebdee923c87edaec652ea0c6c9a5f7b
GK <weeeeek2@naver.com>
2025-05-02 14:50:43 +0900

    feat: Add Member Entity

--- backend/src/main/java/com/ssafy/project/domain/member/entity/Member.java
+++ backend/src/main/java/com/ssafy/project/domain/member/entity/Member.java
@@ -1,5 +1,25 @@
 package com.ssafy.project.domain.member.entity;
 
-public class Member {
+import com.ssafy.project.domain.common.entity.BaseEntity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper=false)
+public class Member extends BaseEntity{
+	private @NonNull Long memberId;
+	private @NonNull String email;
+	private @NonNull String role;
+	private @NonNull String name;
+	private String profileImage;
+	private @NonNull Integer storage;
+	private Long mainBadgeId;
 }
