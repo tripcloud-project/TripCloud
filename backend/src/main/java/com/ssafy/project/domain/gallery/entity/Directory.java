8ce28bd27a5c81846601d0b18fcba1d8e0eac44f
GK <weeeeek2@naver.com>
2025-05-02 14:46:21 +0900

    feat: Add Gallery Entity - Photo, Directory

--- backend/src/main/java/com/ssafy/project/domain/gallery/entity/Directory.java
+++ backend/src/main/java/com/ssafy/project/domain/gallery/entity/Directory.java
@@ -1,5 +1,21 @@
 package com.ssafy.project.domain.gallery.entity;
 
-public class Directory {
+import com.ssafy.project.domain.common.entity.BaseEntity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper=false)
+public class Directory extends BaseEntity{
+	private @NonNull Long directoryId;
+	private @NonNull String name;
+	private @NonNull String fildPath;
 }
