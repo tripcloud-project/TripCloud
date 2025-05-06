0fdf18a8c52470e08bb1a864268009c86013a0c5
myName <kimsungjin927@g.hongik.ac.kr>
2025-05-07 02:14:37 +0900

    feat: MemberDetailServiceImpl 클래스 작성
    - loadUserByUsername: 사용자 아이디를 통해 UserDetails 생성 후 반환

---
+++ backend/src/main/java/com/ssafy/project/domain/auth/service/MemberDetailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ssafy.project.domain.auth.service;
+
+import com.ssafy.project.domain.member.entity.Member;
+import com.ssafy.project.domain.member.repository.MemberRepository;
+import com.ssafy.project.exception.NotFoundMemberException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberDetailServiceImpl implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Member member = memberRepository.selectById(Long.parseLong(memberId));
+        if (member == null || member.getIsDeleted())
+            throw new NotFoundMemberException();
+        return new MemberDetailImpl(member);
+    }
+}
