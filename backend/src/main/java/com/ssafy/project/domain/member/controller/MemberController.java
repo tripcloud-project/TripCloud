7f923b87115b4e065df595ecbf1096d354ffae07
myName <kimsungjin927@g.hongik.ac.kr>
2025-05-06 22:27:55 +0900

    feat: InvalidPasswordException 작성
    - 유효하지 않은 비밀번호 입력 시 반환

--- backend/src/main/java/com/ssafy/project/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/project/domain/member/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.ssafy.project.domain.member.controller;
 import com.ssafy.project.domain.member.dto.request.MemberRegisterDto;
 import com.ssafy.project.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.javassist.bytecode.DuplicateMemberException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    private ResponseEntity<?> registerMember(@RequestBody MemberRegisterDto memberRegisterDto) {
+    private ResponseEntity<?> registerMember(@RequestBody MemberRegisterDto memberRegisterDto) throws DuplicateMemberException {
         memberService.createMember(memberRegisterDto);
         return ResponseEntity.status(201)
                 .body(createSuccessWithNoContent());
--- backend/src/main/java/com/ssafy/project/exception/ErrorCode.java
+++ backend/src/main/java/com/ssafy/project/exception/ErrorCode.java
@@ -10,7 +10,8 @@ import lombok.Getter;
 public enum ErrorCode {
 
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "TOKEN-001", "유효하지않은 토큰입니다."),
-    NOT_FOUND_USER(HttpStatus.UNAUTHORIZED, "ACCOUNT-001", "이메일 또는 비밀번호를 확인하세요.");
+    NOT_FOUND_USER(HttpStatus.UNAUTHORIZED, "ACCOUNT-001", "이메일 또는 비밀번호를 확인하세요."),
+    DUPLICATE_MEMBER(HttpStatus.CONFLICT, "ACCOUNT-002", "이미 존재하는 회원입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
