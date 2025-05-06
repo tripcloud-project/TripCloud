5580835981596b040515b68c9639d8aeab034215
myName <kimsungjin927@g.hongik.ac.kr>
2025-05-07 02:16:54 +0900

    feat: doNothing 메서드 작성
    - 아무 작동하지 않는 더미 메서드
    - SecurityContextHolder에서 멤버 데이터 가져와서 출력

--- backend/src/main/java/com/ssafy/project/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/project/domain/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package com.ssafy.project.domain.member.controller;
 
+import com.ssafy.project.domain.auth.service.MemberDetailImpl;
 import com.ssafy.project.domain.member.dto.request.MemberRegisterDto;
 import com.ssafy.project.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.javassist.bytecode.DuplicateMemberException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import static com.ssafy.project.common.response.ApiResponse.createSuccessWithNoContent;
 
@@ -23,5 +22,12 @@ public class MemberController {
         memberService.createMember(memberRegisterDto);
         return ResponseEntity.status(201)
                 .body(createSuccessWithNoContent());
+    }
+
+    @GetMapping("/nothing")
+    private ResponseEntity<?> doNothing() {
+        MemberDetailImpl member = (MemberDetailImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println("member = " + member.member());
+        return ResponseEntity.noContent().build();
     }
 }
