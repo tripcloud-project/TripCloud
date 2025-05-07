cec0052c7b0517f9901d31871c3c03ed7a83ee45
김성진 <kimsungjin927@gmail.com>
2025-05-07 15:32:57 +0900

    feat: SQL 질의들 authMapper.xml로 분리

--- backend/src/main/java/com/ssafy/project/domain/auth/mapper/AuthMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/auth/mapper/AuthMapper.java
@@ -1,16 +1,11 @@
 package com.ssafy.project.domain.auth.mapper;
 
-import com.ssafy.project.domain.auth.dto.request.LoginRequestDto;
-import com.ssafy.project.domain.member.entity.Member;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+
+import com.ssafy.project.domain.auth.dto.response.LoginResponseDto;
 
 
 @Mapper
 public interface AuthMapper {
-
-    @Select("SELECT u.id, email, password, u.name, r.name AS 'role', created_at, updated_at, deleted_at\r\n"
-            + "FROM users u \r\n"
-            + "    JOIN roles r on u.role_id = r.id WHERE email=#{email} AND password=#{password}")
-    Member selectByEmailAndPassword(LoginRequestDto loginRequest);
+    LoginResponseDto selectByEmail(String email);
 }
