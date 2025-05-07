bc8c445f135880cfe10d13eca462b7c93e0c73ab
김성진 <kimsungjin927@gmail.com>
2025-05-07 16:38:16 +0900

    fix: 잘못된 import 수정

--- backend/src/main/java/com/ssafy/project/domain/auth/service/MemberDetailsService.java
+++ backend/src/main/java/com/ssafy/project/domain/auth/service/MemberDetailsService.java
@@ -1,13 +1,15 @@
 package com.ssafy.project.domain.auth.service;
 
-import com.ssafy.project.domain.member.entity.Member;
-import com.ssafy.project.domain.member.repository.MemberRepository;
-import com.ssafy.project.exception.NotFoundMemberException;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import com.ssafy.project.domain.member.entity.Member;
+import com.ssafy.project.domain.member.exception.NotFoundMemberException;
+import com.ssafy.project.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
