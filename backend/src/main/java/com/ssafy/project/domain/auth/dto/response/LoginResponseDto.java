27b922e9e072e1cc45ca949861abe6917c2f3c63
김성진 <kimsungjin927@gmail.com>
2025-05-07 14:41:38 +0900

    feat: 필수 필드 복구

--- backend/src/main/java/com/ssafy/project/domain/auth/dto/response/LoginResponseDto.java
+++ backend/src/main/java/com/ssafy/project/domain/auth/dto/response/LoginResponseDto.java
@@ -10,11 +10,11 @@ import lombok.NonNull;
 @NoArgsConstructor // 기본 생성자
 @AllArgsConstructor // 파라미터 생성자
 @Builder // 빌터 패턴을 적용
-public class LoginResponse {
-    private @NonNull Long memberId;
-    private @NonNull String email;
-    private @NonNull String name;
-    private @NonNull String role;
+public class LoginResponseDto {
+	private @NonNull Long memberId;
+	private @NonNull String email;
+	private @NonNull String name;
+	private @NonNull String role;
     private String accessToken; // 모든 과정 인증에 사용 (15분 ~ 30분)
     private String refreshToken; // AccessToken이 만료되었을 때, 재발급에 사용 (AccessToken보다 더 긴 만료일)
 }
