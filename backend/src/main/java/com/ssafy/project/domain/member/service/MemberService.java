41b27acd7ca92afaaf6d50c472083745b4da39a1
nnijgnus <kimsungjin927@g.hongik.ac.kr>
2025-05-08 00:15:11 +0900

    feat: 중복 이메일 체크 메서드 작성

--- backend/src/main/java/com/ssafy/project/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/project/domain/member/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.ssafy.project.domain.member.service;
 
 import com.ssafy.project.domain.member.dto.request.MemberRegisterDto;
+import com.ssafy.project.domain.member.dto.response.ValidateEmailResponseDto;
 
 public interface MemberService {
     void createMember(MemberRegisterDto memberRegisterDto);
+    ValidateEmailResponseDto isEmailDuplicated(String email);
 }
