804ac3aedce35322ea77529c0bbdcb3316b6a241
김성진 <kimsungjin927@gmail.com>
2025-05-07 17:33:31 +0900

    refactor: 메서드 이름 errorResponse로 수정
    - createError 메서드가 이미 존재함

--- backend/src/main/java/com/ssafy/project/exception/ControllerExceptionHandler.java
+++ backend/src/main/java/com/ssafy/project/exception/ControllerExceptionHandler.java
@@ -17,40 +17,41 @@ import com.ssafy.project.domain.member.exception.NotFoundMemberException;
 @RestControllerAdvice(annotations = RestController.class) // @RestController Annotation이 붙은 모든 클래스 대상.
 public class ControllerExceptionHandler {
 
-    private ResponseEntity<?> createError(ErrorCode code){
+    private ResponseEntity<?> errorResponse(ErrorCode code){
         ApiResponse<?> errorResponse = ApiResponse.createError(code.getMessage());
         return ResponseEntity
                 .status(code.getHttpStatus())
                 .body(errorResponse);
     }
+    
     // 어떤 예외가 발생하면 어떻게 응답을 해줄지.
     @ExceptionHandler(value = {NotFoundMemberException.class})
     public ResponseEntity<?> notFoundUserException() {
-    	return createError(ErrorCode.NOT_FOUND_USER);
+    	return errorResponse(ErrorCode.NOT_FOUND_USER);
     }
 
     // 키가 중복되는 멤버 삽입 시도
     @ExceptionHandler(value = {DuplicateMemberException.class})
     public ResponseEntity<?> duplicateUserException() {
-    	return createError(ErrorCode.DUPLICATE_MEMBER);
+    	return errorResponse(ErrorCode.DUPLICATE_MEMBER);
     }
 
     // 유효하지 않은 비밀번호 입력
     @ExceptionHandler(value = {InvalidPasswordException.class})
     public ResponseEntity<?> invalidPasswordException() {
-    	return createError(ErrorCode.INVALID_PASSWORD);
+    	return errorResponse(ErrorCode.INVALID_PASSWORD);
     }
 
     // 파일 업로드 예외
     @ExceptionHandler(UploadFailException.class)
     public ResponseEntity<?> handleUploadFailException(UploadFailException e) {
-    	return createError(ErrorCode.UPLOAD_FAIL);
+    	return errorResponse(ErrorCode.UPLOAD_FAIL);
     }
     
     // 유효하지 않은 토큰 사용
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<?> invalidTokenException() {
-    	return createError(ErrorCode.INVALID_TOKEN);
+    	return errorResponse(ErrorCode.INVALID_TOKEN);
     }
     
 
