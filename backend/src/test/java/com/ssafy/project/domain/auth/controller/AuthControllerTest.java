f5c5d311962638d55d33eacc38a066124a4cc03f
김성진 <kimsungjin927@gmail.com>
2025-05-07 14:38:52 +0900

    test: 로그인 성공 테스트 작성

--- backend/src/test/java/com/ssafy/project/domain/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/ssafy/project/domain/auth/controller/AuthControllerTest.java
@@ -1,45 +1,75 @@
 package com.ssafy.project.domain.auth.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.project.domain.auth.dto.request.LoginRequestDto;
+import com.ssafy.project.domain.auth.dto.response.LoginResponseDto;
+import com.ssafy.project.domain.auth.service.AuthService;
+import com.ssafy.project.util.JWTUtil;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(AuthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class AuthControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
+    
+    @MockBean
+    private AuthService authService;
+    @MockBean
+    private JWTUtil jwtUtil;
+    
+    private final String API_PATH = "/api/v1/auth";
+
+    ResultActions doPost(String endPoint, String body) throws Exception {
+        return mockMvc.perform(post(API_PATH + endPoint)
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content(body));
+    }
 
     @Test
-    void testCreateBoard() throws Exception {
+    @DisplayName("로그인 성공 테스트")
+    void loginTest_Success() throws Exception {
         // given
-        Map<String, String> requestBody = new HashMap<>();
-        requestBody.put("email", "test@example.com");
-        requestBody.put("password", "test1234");
+        String email = "test@example.com";
+        String password = "testPassword";
+        LoginRequestDto loginRequestDto = new LoginRequestDto(email, password);
+        String requestBody = objectMapper.writeValueAsString(loginRequestDto);
 
-        String json = objectMapper.writeValueAsString(requestBody);
+        String accessToken = "accessToken";
+        String refreshToken = "refreshToken";
+        LoginResponseDto loginResponseDto = new LoginResponseDto(accessToken, refreshToken);
+        
+        given(authService.login(loginRequestDto)).willReturn(loginResponseDto);
 
-        // when & then
-        mockMvc.perform(post("/api/v1/auth/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isOk())
-                .andDo(print()); // or isCreated(), depending on your controller
+        // when
+        ResultActions perform = doPost("/login", requestBody);
+        
+        // then
+        perform.andExpect(status().isOk())
+        			.andExpect(header().string(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken))
+        			.andExpect(cookie().value("refresh_token", refreshToken))
+        			.andDo(print());
     }
 }
