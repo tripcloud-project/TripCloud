077a831b02c0697639950e22aecdfdc9c8cbb3ff
nnijgnus <kimsungjin927@g.hongik.ac.kr>
2025-05-08 01:22:48 +0900

    feat: 내정보 조회 API 추가

--- backend/src/main/java/com/ssafy/project/domain/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/project/domain/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import static com.ssafy.project.common.response.ApiResponse.createSuccess;
 import static com.ssafy.project.common.response.ApiResponse.createSuccessWithNoContent;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
     private final MemberService memberService;
 
+    // 회원가입
     @PostMapping
     private ResponseEntity<?> registerMember(@RequestBody MemberRegisterDto memberRegisterDto) {
         memberService.createMember(memberRegisterDto);
@@ -22,9 +24,17 @@ public class MemberController {
                 .body(createSuccessWithNoContent());
     }
 
+    // 이메일 중복 검사
     @GetMapping("/checkEmail")
     private ResponseEntity<?> checkEmail(@RequestParam String email) {
         return ResponseEntity.status(200)
                 .body(createSuccess(memberService.isEmailDuplicated(email)));
+    }
+
+    // 마이페이지 조회
+    @GetMapping("/me")
+    private ResponseEntity<?> getMyInformation(Authentication authentication) {
+        return ResponseEntity.status(200)
+                .body(createSuccess(memberService.getCurrentMemberInfo(authentication)));
     }
 }
