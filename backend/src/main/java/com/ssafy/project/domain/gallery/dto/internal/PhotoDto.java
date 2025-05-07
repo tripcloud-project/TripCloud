069472e31aa410e28f4fb710e500939be225d913
GK <weeeeek2@naver.com>
2025-05-07 15:19:17 +0900

    refactor: Gallery entity -> dto 이동

---
+++ backend/src/main/java/com/ssafy/project/domain/gallery/dto/internal/PhotoDto.java
@@ -0,0 +1,36 @@
+package com.ssafy.project.domain.gallery.dto.internal;
+
+import java.time.LocalDateTime;
+
+import com.ssafy.project.domain.common.entity.BaseEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PhotoDto {
+    private Long photoId;
+    private String s3Key;
+    private String filename;
+    private Long size;
+    private String contentType;
+    private String sido;       // 시/도
+    private String sigungu;    // 시/군/구
+    private String eupmyeondong; // 읍/면/동
+    private Double latitude;
+    private Double longitude;
+    private Boolean isDeleted;
+    private LocalDateTime deletedAt;
+    private LocalDateTime takenAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+    
+    // 멤버 id 외래키로 추가 예정
+}
