87221a5913166f0278ba454fbab9d1ae8326d8cd
김성진 <kimsungjin927@gmail.com>
2025-05-09 13:55:40 +0900

    chore: import문 추가

--- backend/src/main/java/com/ssafy/project/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/ssafy/project/domain/member/service/MemberServiceImpl.java
@@ -17,6 +17,7 @@ import com.ssafy.project.domain.member.dto.response.BadgeResponseDto;
 import com.ssafy.project.domain.member.dto.response.MemberResponseDto;
 import com.ssafy.project.domain.member.dto.response.ValidateEmailResponseDto;
 import com.ssafy.project.domain.member.entity.Member;
+import com.ssafy.project.domain.member.exception.BadgeNotFoundException;
 import com.ssafy.project.domain.member.exception.DuplicateMemberException;
 import com.ssafy.project.domain.member.exception.InvalidPasswordException;
 import com.ssafy.project.domain.member.repository.BadgeRepository;
@@ -106,7 +107,7 @@ public class MemberServiceImpl implements MemberService {
             throw new InvalidPasswordException("비밀번호는 8~24자의 영문, 숫자, 특수문자를 포함해야 합니다.");
         }
         
-        Boolean hasBadge = badgeRepository.existsByMemberId(member.getMemberId());
+        Boolean hasBadge = badgeRepository.existsByMemberIdAndBadgeId(member.getMemberId(), requestDto.getMainBadgeId());
         
         // 보유하지 않은 칭호로 변경 시도
         if(!hasBadge) {
@@ -114,6 +115,6 @@ public class MemberServiceImpl implements MemberService {
         }
         
         requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-        memberRepository.updateMember(requestDto);
+        memberRepository.updateMember(member.getMemberId(), requestDto);
 	}
 }
