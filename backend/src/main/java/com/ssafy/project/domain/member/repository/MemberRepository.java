51e5597da308de02c46956b65eedd900d8a52424
김성진 <kimsungjin927@gmail.com>
2025-05-09 13:54:13 +0900

    feat: updateMember 작성

--- backend/src/main/java/com/ssafy/project/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/project/domain/member/repository/MemberRepository.java
@@ -1,11 +1,14 @@
 package com.ssafy.project.domain.member.repository;
 
 import com.ssafy.project.domain.member.dto.request.MemberRegisterDto;
+import com.ssafy.project.domain.member.dto.request.MemberUpdateRequestDto;
 import com.ssafy.project.domain.member.entity.Member;
+
 import lombok.NonNull;
 
 public interface MemberRepository {
     void insertMember(MemberRegisterDto memberRegisterDto);
     Boolean existsByEmail(@NonNull String email);
-    Member selectById(@NonNull Long id); // TODO 테스트 작성 필요
+    Member selectById(@NonNull Long id);
+	void updateMember(Long memberId, MemberUpdateRequestDto requestDto);
 }
