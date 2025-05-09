7d541c1a4d09c9ccb6aa22a61041e2199d1777ef
김성진 <kimsungjin927@gmail.com>
2025-05-09 12:14:11 +0900

    fix: BadgeResponseDto가 badgeId를 포함하도록 수정

--- backend/src/main/java/com/ssafy/project/domain/member/dto/response/BadgeResponseDto.java
+++ backend/src/main/java/com/ssafy/project/domain/member/dto/response/BadgeResponseDto.java
@@ -1,12 +1,17 @@
 package com.ssafy.project.domain.member.dto.response;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class BadgeResponseDto {
+	private @NonNull Long badgeId;
     private @NonNull String name;
     private String description;
     private String image;
