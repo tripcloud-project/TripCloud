2d495d0d4ad3a188b8d59ab9f2d7ae9872a9b624
김성진 <kimsungjin927@gmail.com>
2025-05-09 11:57:51 +0900

    feat: selectAllByMemberId 구현

--- backend/src/main/java/com/ssafy/project/domain/member/mapper/BadgeMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/member/mapper/BadgeMapper.java
@@ -1,9 +1,13 @@
 package com.ssafy.project.domain.member.mapper;
 
-import com.ssafy.project.domain.member.dto.response.BadgeResponseDto;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.ssafy.project.domain.member.dto.response.BadgeResponseDto;
 
 @Mapper
 public interface BadgeMapper {
     BadgeResponseDto selectById(Long badgeId);
+    List<BadgeResponseDto> selectAllByMemberId(Long memberId);
 }
