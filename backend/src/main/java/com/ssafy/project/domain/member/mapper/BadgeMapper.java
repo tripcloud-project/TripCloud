2175b128b6da3d9e029da307c9a15fec04a9d8aa
김성진 <kimsungjin927@gmail.com>
2025-05-09 13:54:58 +0900

    feat: existsByMemberIdAndBadgeId 구현
    - 멤버 아이디와 칭호 아이디를 통해 해당 사용자가 해당 칭호를 보유했는지 반환

--- backend/src/main/java/com/ssafy/project/domain/member/mapper/BadgeMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/member/mapper/BadgeMapper.java
@@ -6,8 +6,11 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.project.domain.member.dto.response.BadgeResponseDto;
 
+import lombok.NonNull;
+
 @Mapper
 public interface BadgeMapper {
     BadgeResponseDto selectById(Long badgeId);
     List<BadgeResponseDto> selectAllByMemberId(Long memberId);
+	Boolean existsByMemberIdAndBadgeId(@NonNull Long memberId, Long badgeId);
 }
