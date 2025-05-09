65518bbfce1976b0bd2cda5b1a17d8e0428c8820
김성진 <kimsungjin927@gmail.com>
2025-05-09 12:19:21 +0900

    feat: MemberUpdateRequestDto 작성
    - 개인정보 수정 요청 시 사용
    - password: 수정할 비밀번호, NonNull
    - name: 수정할 이름
    - profileImage: 수정할 프로필 사진
    - mainBadgeId: 바꿀 대표 칭호의 아이디

---
+++ backend/src/main/java/com/ssafy/project/domain/member/dto/request/MemberUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.project.domain.member.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MemberUpdateRequestDto {
+	private @NonNull String password;
+	private @NonNull String name;
+	private String profileImage;
+	private Long mainBadgeId;
+}
