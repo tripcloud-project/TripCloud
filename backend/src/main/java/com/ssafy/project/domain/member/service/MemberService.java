f329d38407eae0cf13402ea655cf9b82859882e4
김성진 <kimsungjin927@gmail.com>
2025-05-09 14:43:52 +0900

    fix: conflict 해결

--- backend/src/main/java/com/ssafy/project/domain/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/project/domain/member/service/MemberService.java
@@ -1,15 +1,12 @@
 package com.ssafy.project.domain.member.service;
 
-<<<<<<< backend/src/main/java/com/ssafy/project/domain/member/service/MemberService.java
 import org.springframework.security.core.Authentication;
 
-=======
 import java.time.LocalDateTime;
 
 import org.springframework.security.core.Authentication;
 
 import com.ssafy.project.common.response.PageResponse;
->>>>>>> backend/src/main/java/com/ssafy/project/domain/member/service/MemberService.java
 import com.ssafy.project.domain.member.dto.request.MemberRegisterDto;
 import com.ssafy.project.domain.member.dto.response.BadgeListResponseDto;
 import com.ssafy.project.domain.member.dto.response.MemberResponseDto;
