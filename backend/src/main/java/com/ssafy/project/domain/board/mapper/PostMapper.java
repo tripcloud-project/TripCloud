3ddd5a6c9ed5156ced09245d4d628d01f35d7c8c
nnijgnus <kimsungjin927@g.hongik.ac.kr>
2025-05-12 01:18:56 +0900

    feat: delete SQL 작성
    - soft delete 적용

--- backend/src/main/java/com/ssafy/project/domain/board/mapper/PostMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/board/mapper/PostMapper.java
@@ -1,9 +1,11 @@
 package com.ssafy.project.domain.board.mapper;
 
 import com.ssafy.project.domain.board.dto.PostRequestDto;
+import lombok.NonNull;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface PostMapper {
     int insert(PostRequestDto postRequestDto);
+    int delete(@NonNull Long memberId, Long postId);
 }
