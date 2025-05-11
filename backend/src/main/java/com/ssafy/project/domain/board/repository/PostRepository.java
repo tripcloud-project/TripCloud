76f79db263a58f7be312d8245680301d045be362
nnijgnus <kimsungjin927@g.hongik.ac.kr>
2025-05-12 01:18:11 +0900

    feat: delete 메서드 구현
    - 삭제 성공시 true, 실패시 false 반환

--- backend/src/main/java/com/ssafy/project/domain/board/repository/PostRepository.java
+++ backend/src/main/java/com/ssafy/project/domain/board/repository/PostRepository.java
@@ -1,7 +1,9 @@
 package com.ssafy.project.domain.board.repository;
 
 import com.ssafy.project.domain.board.dto.PostRequestDto;
+import lombok.NonNull;
 
 public interface PostRepository {
     void insert(PostRequestDto postRequestDto);
+    boolean delete(@NonNull Long memberId, Long postId);
 }
--- backend/src/main/java/com/ssafy/project/domain/board/repository/PostRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/project/domain/board/repository/PostRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.ssafy.project.domain.board.repository;
 
 import com.ssafy.project.domain.board.dto.PostRequestDto;
 import com.ssafy.project.domain.board.mapper.PostMapper;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +10,14 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class PostRepositoryImpl implements PostRepository {
     private final PostMapper postMapper;
+
     @Override
     public void insert(PostRequestDto postRequestDto) {
         postMapper.insert(postRequestDto);
+    }
+
+    @Override
+    public boolean delete(@NonNull Long memberId, Long postId) {
+        return postMapper.delete(memberId, postId) == 1;
     }
 }
