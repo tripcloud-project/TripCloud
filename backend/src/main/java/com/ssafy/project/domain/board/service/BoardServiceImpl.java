e9b8120447ef792e38972c0547ebf459d29af353
nnijgnus <kimsungjin927@g.hongik.ac.kr>
2025-05-12 00:27:02 +0900

    feat: createPost 메서드 구현

--- backend/src/main/java/com/ssafy/project/domain/board/service/BoardServiceImpl.java
+++ backend/src/main/java/com/ssafy/project/domain/board/service/BoardServiceImpl.java
@@ -1,11 +1,15 @@
 package com.ssafy.project.domain.board.service;
 
+import com.ssafy.project.domain.board.dto.PostRequestDto;
 import com.ssafy.project.domain.board.repository.PostRepository;
 import com.ssafy.project.domain.gallery.exception.UploadFailException;
 import com.ssafy.project.domain.gallery.service.helper.UploadHelper;
+import com.ssafy.project.domain.member.entity.Member;
 import com.ssafy.project.infra.s3.S3Service;
+import com.ssafy.project.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -13,8 +17,18 @@ import java.io.IOException;
 @Service
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService {
+    private final PostRepository postRepository;
     private final S3Service s3Service;
 
+    @Transactional
+    @Override
+    public void createPost(PostRequestDto postRequestDto) {
+        Member member = SecurityUtil.getCurrentMember();
+        postRequestDto.setMemberId(member.getMemberId());
+        postRepository.insert(postRequestDto);
+    }
+
+    @Transactional
     @Override
     public String uploadImage(MultipartFile image) {
         try {
