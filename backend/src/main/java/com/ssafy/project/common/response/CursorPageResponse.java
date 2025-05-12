ed95d2e503f92fe647906b2d9eff0f3ba2ce7f58
김성진 <kimsungjin927@gmail.com>
2025-05-12 16:45:48 +0900

    fix: PageResponse 이름 변경
    - PageResponse > CursorPageResponse

--- backend/src/main/java/com/ssafy/project/common/response/CursorPageResponse.java
+++ backend/src/main/java/com/ssafy/project/common/response/CursorPageResponse.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class PageResponse<T> {
+public class CursorPageResponse<T> {
 	private List<T> content;
 	private boolean hasNext;
 	private Integer size;
