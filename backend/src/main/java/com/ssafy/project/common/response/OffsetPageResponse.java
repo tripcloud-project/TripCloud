3515f199e823f1ee2611bb6b82dadc0c69033701
김성진 <kimsungjin927@gmail.com>
2025-05-12 16:47:29 +0900

    feat: OffsetPageResponse 작성
    - content: 반환 결과
    - hasNext: 다음 페이지 존재 여부
    - size: 현재 페이지의 크기
    - nextPage: 다음 페이지 넘버

---
+++ backend/src/main/java/com/ssafy/project/common/response/OffsetPageResponse.java
@@ -0,0 +1,19 @@
+package com.ssafy.project.common.response;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class OffsetPageResponse<T> {
+	private List<T> content;
+	private boolean hasNext;
+	private Integer size;
+	private Integer nextPage;
+}
