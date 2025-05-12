34ed1cc54a148dbe8985d20a16e61e88d23592bf
김성진 <kimsungjin927@gmail.com>
2025-05-13 01:30:46 +0900

    feat: delete SQL 작성

--- backend/src/main/java/com/ssafy/project/domain/board/mapper/CommentMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/board/mapper/CommentMapper.java
@@ -1,6 +1,7 @@
 package com.ssafy.project.domain.board.mapper;
 
 import com.ssafy.project.domain.board.dto.response.CommentResponseDto;
+import lombok.NonNull;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.ssafy.project.domain.board.dto.request.CommentRequestDto;
@@ -12,4 +13,6 @@ public interface CommentMapper {
     int insert(CommentRequestDto commentRequestDto);
 
     List<CommentResponseDto> selectByPostId(Long postId, Long memberId);
+
+    int delete(@NonNull Long memberId, Long postId, Long commentId);
 }
