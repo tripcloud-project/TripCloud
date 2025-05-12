3650cd3bc61cdcb256c4eb99fbc0f7643a0f3fe4
김성진 <kimsungjin927@gmail.com>
2025-05-12 17:49:14 +0900

    feat: PostPreviewResponseDto 작성
    게시글 미리보기
    - postId: 게시글 아이디
    - title: 게시글 제목
    - author: 작성자 이름
    - createdAt: 작성일자

---
+++ backend/src/main/java/com/ssafy/project/domain/board/dto/response/PostPreviewResponseDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.project.domain.board.dto.response;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostPreviewResponseDto {
+	private @NonNull Long postId;
+	private @NonNull String title;
+	private @NonNull String author;
+	private @NonNull LocalDateTime createdAt;
+}
