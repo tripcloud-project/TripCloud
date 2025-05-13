ae15d1dd6375d300de69146d8af88935f749b539
김성진 <kimsungjin927@gmail.com>
2025-05-13 17:10:14 +0900

    feat: searchByKeyword 구현

--- backend/src/main/java/com/ssafy/project/domain/gallery/service/FileService.java
+++ backend/src/main/java/com/ssafy/project/domain/gallery/service/FileService.java
@@ -2,12 +2,17 @@ package com.ssafy.project.domain.gallery.service;
 
 import java.util.List;
 
-import com.ssafy.project.domain.gallery.dto.internal.DownloadDto;
-import com.ssafy.project.domain.gallery.dto.request.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.ssafy.project.domain.gallery.dto.internal.DownloadDto;
+import com.ssafy.project.domain.gallery.dto.request.DeleteRequestDto;
+import com.ssafy.project.domain.gallery.dto.request.DirectoryRenameRequestDto;
+import com.ssafy.project.domain.gallery.dto.request.DownloadRequestDto;
+import com.ssafy.project.domain.gallery.dto.request.RestoreRequestDto;
+import com.ssafy.project.domain.gallery.dto.request.TrashRequestDto;
 import com.ssafy.project.domain.gallery.dto.response.DirectoryResponseDto;
 import com.ssafy.project.domain.gallery.dto.response.FileDetailResponseDto;
+import com.ssafy.project.domain.gallery.dto.response.SearchResultResponseDto;
 
 public interface FileService {
 	void uploadFiles(List<MultipartFile> files, String prefix);
@@ -19,4 +24,5 @@ public interface FileService {
 	void trashBulk(TrashRequestDto trashRequestDto);
 	void restore(RestoreRequestDto restoreRequestDto);
 	void delete(DeleteRequestDto deleteRequestDto);
+	SearchResultResponseDto searchByKeyWord(String keyword);
 }
