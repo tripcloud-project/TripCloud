1f0fa10fbf07404c0dabfdfb92914fde9cd043f6
김성진 <kimsungjin927@gmail.com>
2025-05-16 12:26:19 +0900

    feat: updateDescription SQL 작성

--- backend/src/main/java/com/ssafy/project/domain/gallery/mapper/PhotoMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/gallery/mapper/PhotoMapper.java
@@ -1,14 +1,17 @@
 package com.ssafy.project.domain.gallery.mapper;
 
-import com.ssafy.project.domain.gallery.dto.internal.DirectoryEntry;
-import com.ssafy.project.domain.gallery.dto.internal.FileEntry;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.List;
+import com.ssafy.project.domain.gallery.dto.internal.DirectoryEntry;
+import com.ssafy.project.domain.gallery.dto.internal.FileEntry;
+import com.ssafy.project.domain.gallery.dto.request.PhotoDescriptionRequestDto;
 
 @Mapper
 public interface PhotoMapper {
     List<DirectoryEntry> findAllSidoByMemberId(Long memberId);
     List<DirectoryEntry> findSigunguBySidoAndMemberId(String sido, Long memberId);
     List<FileEntry> findPhotosBySidoAndSigunguAndMemberId(String sido, String sigungu, Long memberId);
+	int updateDescription(PhotoDescriptionRequestDto requestDto);
 }
