e23efdedc8910dbbcf5019a17cf3a8995c253e04
김성진 <kimsungjin927@gmail.com>
2025-05-16 15:20:38 +0900

    feat: selectByMemberIdAndPhotoId, updateThumbnail 구현
    - selectByMemberIdAndPhotoId: 멤버 아이디와 사진 아이디로 사진 객체 반환
    - updateThumbnail: 이미 해당 지역에 섬네일이 있다면 수정, 없다면 삽입

--- backend/src/main/java/com/ssafy/project/domain/gallery/mapper/PhotoMapper.java
+++ backend/src/main/java/com/ssafy/project/domain/gallery/mapper/PhotoMapper.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ssafy.project.domain.board.dto.request.ThumbnailRequestDto;
 import com.ssafy.project.domain.gallery.dto.internal.DirectoryEntry;
 import com.ssafy.project.domain.gallery.dto.internal.FileEntry;
 import com.ssafy.project.domain.gallery.dto.request.PhotoDescriptionRequestDto;
+import com.ssafy.project.domain.gallery.dto.response.FileDetailResponseDto;
 
 @Mapper
 public interface PhotoMapper {
@@ -14,4 +16,6 @@ public interface PhotoMapper {
     List<DirectoryEntry> findSigunguBySidoAndMemberId(String sido, Long memberId);
     List<FileEntry> findPhotosBySidoAndSigunguAndMemberId(String sido, String sigungu, Long memberId);
 	int updateDescription(PhotoDescriptionRequestDto requestDto);
+	FileDetailResponseDto selectByMemberIdAndPhotoId(Long memberId, Long photoId);
+	int updateThumbnail(ThumbnailRequestDto requestDto);
 }
