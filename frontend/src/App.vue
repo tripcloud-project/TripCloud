46d135137f95bd72cc4134544b89931eeaab471d
김성진 <kimsungjin927@gmail.com>
2025-05-22 10:58:53 +0900

    feat: 임시 nav 작성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,11 +5,10 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/login">로그인</RouterLink>
+      <RouterLink to="/register">회원가입</RouterLink>
     </nav>
   </header>
-
   <RouterView />
 </template>
 
