403d63d4928ce2f8bdd8fe150067f52c28f9139a
김성진 <kimsungjin927@gmail.com>
2025-05-22 10:59:39 +0900

    feat: /login 경로에 로그인 뷰 라우팅

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView,
-    // },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView,
+    },
   ],
 })
 
