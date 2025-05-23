2a5fdbea7f3966c5538438898d8b0a0689188eea
김성진 <kimsungjin927@gmail.com>
2025-05-23 17:58:49 +0900

    feat: 라우터 추가

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,7 @@ import RegisterView from '@/views/RegisterView.vue'
 import DriveView from '@/views/DriveView.vue'
 import BoardListView from '@/views/BoardListView.vue'
 import PostDetailView from '@/views/PostDetailView.vue'
+import NewPostView from '@/views/NewPostView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -33,6 +34,11 @@ const router = createRouter({
       name: 'posts',
       component: PostDetailView,
       props: true,
+    },
+    {
+      path: '/posts/new',
+      name: 'newPost',
+      component: NewPostView,
     },
   ],
 })
