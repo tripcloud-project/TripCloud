7029bc131c9b9d4ffa8187075e048bc6ea20a4f5
GK <weeeeek2@naver.com>
2025-05-23 11:08:30 +0900

    feat: DriveView 추가 및 라우터 설정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
+import DriveView from '@/views/DriveView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'register',
       component: RegisterView,
     },
+    {
+      path: '/drive',
+      name: 'drive',
+      component: DriveView
+    }
   ],
 })
 
