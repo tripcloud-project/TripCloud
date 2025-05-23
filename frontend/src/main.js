7d5003e41f3dfb221d7c8753e4fb191a05ac3cfe
김성진 <kimsungjin927@gmail.com>
2025-05-23 17:58:14 +0900

    feat: quill 등록

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { registerQuill } from './lib/quillSetup'
+import { useAuthStore } from '@/stores/useAuthStore'
+
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import '@/assets/tailwind.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 import App from './App.vue'
 import router from './router'
 
-import { useAuthStore } from '@/stores/useAuthStore'
-
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
+registerQuill(app)
 
 app.mount('#app')
 
