1285c894efbe7e553a04bcc6b98e9f20805eb164
GK <weeeeek2@naver.com>
2025-05-23 11:08:55 +0900

    feat: axios 해더에 토큰 추가

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -8,4 +8,41 @@ const api = axios.create({
   },
 })
 
+
+
+import { useAuthStore } from '@/stores/useAuthStore'
+api.interceptors.request.use(
+  async (config) => {
+    console.log('[요청 발신]: ', config.method, config.url, config.data)
+    const authStore = useAuthStore();
+    console.log("authStore.token: ", authStore.token)
+
+
+    if(authStore.token){
+      config.headers['Authorization'] = `${authStore.token}`
+    }
+    return config
+  },
+  (error) => {
+    console.log('[요청 실패]: ', error)
+    return Promise.reject(error)
+  },
+)
+
+
+api.interceptors.response.use(
+  (response) => {
+    console.log(response)
+    console.log('[응답 수신 1]: ', response.status, response.data)
+    return response
+  },
+  async (error) => {
+    console.log('[오류 수신 1]: ', error)
+    return Promise.reject(error)
+  },
+)
+
+
+
+
 export default api
