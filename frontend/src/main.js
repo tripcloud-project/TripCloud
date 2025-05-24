ee37a57a93ac32d1f14cbadf9f703f45358a366a
GK <weeeeek2@naver.com>
2025-05-24 15:15:37 +0900

    feat: Add pinia-plugin-persistedstate

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,9 +10,11 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import App from './App.vue'
 import router from './router'
 
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 const app = createApp(App)
 const pinia = createPinia()
-
+pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
 registerQuill(app)
