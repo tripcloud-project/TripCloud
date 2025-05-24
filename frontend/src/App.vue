7fb31ca29a0286074031e1dd5314aedb8299c613
김성진 <kimsungjin927@gmail.com>
2025-05-25 03:47:23 +0900

    feat: 지도 라우터 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/drive-test">드라이브 테스트</RouterLink>
       <RouterLink to="/board">게시판</RouterLink>
       <RouterLink to="/posts/new">새 글 쓰기</RouterLink>
+      <RouterLink to="/drive/map">지도 보기</RouterLink>
     </nav>
   </header>
   <RouterView />
