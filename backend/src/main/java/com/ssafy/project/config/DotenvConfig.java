b26c035905d17c8c1bd38729030e878fade711dc
GK <weeeeek2@naver.com>
2025-05-29 01:18:21 +0900

    feat: dotenv 의존성 및 config 추가

---
+++ backend/src/main/java/com/ssafy/project/config/DotenvConfig.java
@@ -0,0 +1,15 @@
+package com.ssafy.project.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DotenvConfig {
+    static {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+    }
+}
+
